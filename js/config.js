637ce518e49c4c461d570547343d91bfa7c040d3
rastogianshul85 <76913400+rastogianshul85@users.noreply.github.com>
2021-01-06 22:12:45 +0100

    updated title

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-1_Lzo0TG9Nq', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5qi4dfghrmj8p5573g3oefmkgf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-1' // e.g. us
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
